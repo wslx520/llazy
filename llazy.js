c5ecb83d8747b6fe21f41659da2d13ebb51f5c82
李兴 <lixing@jingoal.com>
2017-03-08 14:13:17 +0800

    完善；加入水平滚动示例；在html中写入思路

--- llazy.js
+++ llazy.js
@@ -1,10 +1,8 @@
 var llazy = (function(doc) {
     // 判断两个矩形是否相交
     function cross(r1, r2) {
-        if (Math.abs((r1.left + r1.right) / 2 - (r2.left + r2.right) / 2) < ((r1.right + r2.right - r1.left - r2.left) / 2)
-            && Math.abs((r1.top + r1.bottom) / 2 - (r2.top + r2.bottom) / 2) < ((r1.bottom + r2.bottom - r1.top - r2.top) / 2))
-            return true;
-        return false;
+        return (Math.abs((r1.left + r1.right) / 2 - (r2.left + r2.right) / 2) < ((r1.right + r2.right - r1.left - r2.left) / 2)
+            && Math.abs((r1.top + r1.bottom) / 2 - (r2.top + r2.bottom) / 2) < ((r1.bottom + r2.bottom - r1.top - r2.top) / 2));
     }
     var addEvent = doc.addEventListener ? function(dom, type, listener) {
         dom.addEventListener(type, listener, false);
@@ -16,52 +14,63 @@ var llazy = (function(doc) {
         for (;i < len; res.push(arrayLike[i++]));
         return res;
     };
-    var LLazy = function(container, ops) {
-        container = container || doc.body;
-        var imgs = container.getElementsByTagName('img');
-        imgs = toArray(imgs);
-        var onload = function(e) {
-            e = e || window.event;
-            var img = e.target || e.srcElement;
-            img._loaded = true;
-        };
+    // 表示“载入”状态，用随机数确保不会与 img 其他自定义属性冲突
+    var tempLoading = Math.random() + '_loading';
+    var LLazy = function(imgs, ops) {
+        ops = ops || {};
+        imgs = typeof imgs === 'string' ? doc.querySelectorAll(imgs) : imgs;
+        if (!imgs.splice) {
+            imgs = toArray(imgs);
+        }
+        var container = ops.container || doc.documentElement;
+        if (typeof container === 'string') {
+            container = doc.querySelector(container);
+        }
+        var originSrc = ops.src || 'data-src';
         var onerror = function(e) {
             e = e || window.event;
             var img = e.target || e.srcElement;
             img.src = img.getAttribute('data-src');
         };
+        // console.log(imgs, container);
         // 用 timer 做一个简单的函数节流阀
         var _timer = null;
         var onscroll = function(e) {
-            console.log('scrolling');
+            // console.log('scrolling');
             clearTimeout(_timer);
             _timer = setTimeout(function () {
                 var t = Date.now();
-                console.log('do scrolling todo', t);
+                // console.log('do scrolling todo', t);
+                // window 没有 getBoundingClientRect 方法，body 与 root 才有
                 var cont = container.getBoundingClientRect();
-                console.log(cont);
-                for (var i = 0; i < imgs.length; i++) {
-                    var img = imgs[i];
-                    var rect = img.getBoundingClientRect();
+                // console.log(cont);
+                for (var i = 0, img, rect; i < imgs.length; i++) {
+                    img = imgs[i];
+                    rect = img.getBoundingClientRect();
                     // 在循环中输出内容到控制台，会使最终时间差变得好多倍（FF, IE）
                     // console.log(cross(cont, rect));
                     // 如果图片出现在可见区，则加载，并将已处于 loading 状态的图片移出待加载列表
-                    if (!img._loading && cross(cont, rect)) {
-                        img.src = img.getAttribute('data-src');
-                        img._loading = true;
+                    if (!img[tempLoading] && cross(cont, rect)) {
+                        img.src = img.getAttribute(originSrc);
+                        img[tempLoading] = true;
                         // 移出待加载数组
                         imgs.splice(i, 1);
                     }
                 }
-                console.log(Date.now() -t);
+                // console.log(Date.now() -t);
             }, 50);
             
         };
-        addEvent(container, 'scroll', onscroll);
+        // document.documentElement 并没有 onscroll事件；document.body 的 onscroll 兼容性也不好
+        // 所以，默认 container 是 document.documentElement 或 body 时，则将 onscroll 监听加在 window 上
+        addEvent((container === doc.documentElement || container === doc.body) ? window : container, 'scroll', onscroll);
+        // 初始化时就执行一次 load ，加载出此时已经出现在“可视区”的图片
+        onscroll();
     };
     LLazy.isCross = cross;
     return LLazy;
 })(document);
 
-
-llazy(document.querySelector('#cont'));
+// document.documentElement.onscroll = function (e) {
+//     console.log('sssssssssssscroll')
+// }
